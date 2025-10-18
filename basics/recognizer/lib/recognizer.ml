cd5106d9ee968caae858d6e7f2e2d7b926df6f2f
rikiatzori <rikiatzori99@gmail.com>
2025-10-18 13:00:47 +0200

    Implement regexp recognizer for 5 languages

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,73 +1,46 @@
 (* [01]+ *)
-
-let rec lang1 = function
-    [] -> false
-  | ['0'] | ['1'] -> true
-  | '0'::l -> lang1 l
-  | '1'::l -> lang1 l
-  | _ -> false
+let rec group1 l =
+  match l with
+  | [] -> false 
+  | [x] -> if x = '0' || x = '1' then true else false  
+  | x :: rest -> if x = '0' || x = '1' then group1 rest else false  
 
 (* 0?1* *)
-
-let step2 q a = match q with
-    0 when a='0' || a='1' -> 1
-  | 1 when a='1' -> 1
-  | _ -> -1
-    
-let lang2 w = match List.fold_left step2 0 w with
-    0 | 1 -> true
-  | _ -> false
-
+let group2 l =
+  match l with
+  | [] -> true 
+  | [x] -> if x='0' || x='1' then true else false
+  | '0':: rest -> List.for_all(fun c-> c='1') rest
+  | '1':: rest -> List.for_all(fun c-> c='1') rest
+  |_-> false
 
 (* 0[01]*0 *)
+let group3 l = 
+  if List.hd l = '0' && List.hd (List.rev l ) = '0'then 
+  match List.length l with
+  |x when x>2->  List.for_all(fun c -> c='1' || c='0') l
+  |x when x=2 -> true
+  |_-> false
+else false
 
-let step3 q a = match q with
-    0 when a='0' -> 1
-  | 1 when a='0' -> 2
-  | 1 when a='1' -> 1
-  | 2 when a='0' -> 2
-  | 2 when a='1' -> 1    
-  | _ -> -1
-
-let lang3 w = match List.fold_left step3 0 w with
-    2 -> true
-  | _ -> false
 
 (* 0*10*10* *)
+let group4 l =
+  match l with
+  |[] -> false
+  |[x;y] -> if x='1' && y='1' then true else false
+  |_ -> if List.fold_left (fun acc x -> if x='1' then acc+1 else acc ) 0 l = 2 then true else false
 
-let step4 q a = match q with
-    0 when a='0' -> 0
-  | 0 when a='1' -> 1
-  | 1 when a='0' -> 1
-  | 1 when a='1' -> 2
-  | 2 when a='0' -> 2
-  | 2 when a='1' -> -1
-  | _ -> -1
-
-let lang4 w = match List.fold_left step4 0 w with
-    2 -> true
-  | _ -> false
-
-let step5 q a = match q with
-    0 when a='0' -> 1
-  | 0 when a='1' -> 4
-  | 1 when a='0' -> 2
-  | 2 when a='0' -> 3
-  | 2 when a='1' -> 6
-  | 3 when a='0' -> 2
-  | 4 when a='1' -> 5
-  | 5 when a='0' -> 3
-  | 5 when a='1' -> 6   
-  | 6 when a='1' -> 5   
-  | _ -> -1
 
 (* (00|11)+ *)
-let lang5 w = match List.fold_left step5 0 w with
-    2 | 5 -> true
-  | _ -> false
+let rec group5 l = 
+  match l with
+  |[]-> false
+  |[x;y] -> if (x='0' && y='0') || (x='1' && y='1') then true else false 
+  |x::y::rest -> if (x='0' && y='0') || (x='1' && y='1') then group5 rest else false
+  |_-> false
 
     
-let recognizers = [lang1;lang2;lang3;lang4;lang5]
                   
-let belongsTo w = List.map (fun f -> f w) recognizers
+let belongsTo l = [group1 l;group2 l;group3 l; group4 l;group5 l]
   
